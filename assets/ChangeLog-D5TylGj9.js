ed2e071240c9157b7bb3cb06a7768abef8736cc4
Kevin <kk2229.yang@gmail.com>
2024-07-03 18:43:17 +0800

    Updates

---
+++ assets/ChangeLog-D5TylGj9.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-DWbWqHZ-.js";import{useMDXComponents as r}from"./index-DbIxU3Ed.js";import{M as i,h as m}from"./index-BMGINi5o.js";import"./index-l2PZgWEW.js";import"./iframe-B5464o-6.js";import"../sb-preview/runtime.js";import"./index-CaNG7YX3.js";import"./index-B5xYo-Cg.js";import"./index-DrFu-skq.js";const s=`## V1.0.0 (Mon July 1 2024)
+
+✨ New Feature
+
+- Add Button Story
+
+---
+
+🪲 Bug Fix
+
+- Fix Button Hover Status
+`;function e(t){const o={h1:"h1",...r(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(i,{title:"Changelog"}),`
+`,n.jsx(o.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(m,{children:s})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
---
+++ assets/preview-DsWLmgau.js
@@ -0,0 +1,1 @@
+const o={parameters:{options:{storySort:{method:"",order:["Components","Intro"],locales:""}}},tags:["autodocs"]};export{o as default};
