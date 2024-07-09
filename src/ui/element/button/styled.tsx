205a835f1bd8d15726b8fe2cfbc4bc2c69f592ac
Kevin <kk2229.yang@gmail.com>
2024-07-09 23:41:36 +0800

    🆙 update : modify button fundation

--- src/ui/element/button/styled.tsx
+++ src/ui/element/button/styled.tsx
@@ -1,5 +1,3 @@
-export const getStyle = (variant: string) => {
-  return variant === 'contained'
-    ? 'button-contained-primary'
-    : 'button-outline-primary';
+export const getStyle = (variant: string, themeColor: string) => {
+  return `button-${variant} button-${variant}-${themeColor}`;
 };
