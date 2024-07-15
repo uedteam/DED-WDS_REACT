be582466a4d456df94f74329ce3c184fe6b8049a
Kevin <kk2229.yang@gmail.com>
2024-07-15 23:59:12 +0800

    ✨新增： add textarea component

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,6 @@
-import Button from './element/button/Button';
-import Input from './element/input/Input';
-import Title from './element/title/Title';
+import Button from './element/button/button';
+import Input from './element/input/input';
+import Title from './element/title/title';
+import Textarea from './element/textarea/textarea';
 
-export { Button, Input, Title };
+export { Button, Input, Title, Textarea };
