efe095d83a5647ae7f147d33d2987d34eed52f45
Kevin <kk2229.yang@gmail.com>
2024-07-16 00:22:34 +0800

    Updates

---
+++ assets/ChangeLog-mrdNGjUb.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-DWbWqHZ-.js";import{useMDXComponents as r}from"./index-DbIxU3Ed.js";import{M as i,h as a}from"./index-BeDz1HxD.js";import"./index-l2PZgWEW.js";import"./iframe-CqN4GESq.js";import"../sb-preview/runtime.js";import"./index-CaNG7YX3.js";import"./index-B5xYo-Cg.js";import"./index-DrFu-skq.js";const m=`## V1.0.0 (Mon July 16 2024)
+
+✨ New Feature
+
+- Add Button Story
+- Add Title Story
+- Add Input Story
+- Add Textarea Story
+
+---
+
+🪲 Bug Fix
+
+- Fix Button Hover Status
+`;function e(t){const o={h1:"h1",...r(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(i,{title:"Changelog"}),`
+`,n.jsx(o.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(a,{children:m})]})}function j(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{j as default};
