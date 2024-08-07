51ce1da36a6e58246ca345709d141f1c5eb5e400
Kevin <kk2229.yang@gmail.com>
2024-08-07 09:25:19 +0800

    🆙 update :  新增 icon index 及 顯示現有 icon 清單

---
+++ libs/src/ui/token/icons.ts
@@ -0,0 +1,7 @@
+import AccountIcon from '@assets/account_circle.svg?react';
+import AddIcon from '@assets/add.svg?react';
+import ArrowDownIcon from '@assets/arrow_down.svg?react';
+import CheckIcon from '@assets/check.svg?react';
+import SearchIcon from '@assets/search.svg?react';
+
+export { AccountIcon, AddIcon, ArrowDownIcon, CheckIcon, SearchIcon };
