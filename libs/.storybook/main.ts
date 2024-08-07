29cb064f9c354d393c69ecffcf8e09061745b798
Kevin <kk2229.yang@gmail.com>
2024-08-07 09:23:21 +0800

    🆙 update : 排序設定

--- libs/.storybook/main.ts
+++ libs/.storybook/main.ts
@@ -1,7 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../src/ui/token/*.mdx',
+    '../src/ui/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+  ],
 
   addons: [
     '@storybook/addon-essentials',
