9a64aa0aef815c816476df94b8db1689c7648469
Kevin <kk2229.yang@gmail.com>
2024-08-11 01:59:42 +0800

    ✨新增： add avatar group component

--- libs/src/ui/index.ts
+++ libs/src/ui/index.ts
@@ -5,5 +5,19 @@ import { Textarea } from './element/textarea';
 import { Toggle } from './element/toggle';
 import { Radio } from './element/radio';
 import { Breadcrumb } from './element/breadcrumb';
+import { Avatar } from './element/avatar';
+import { AvatarGroup } from './module/avatar-group';
+import { Menu } from './module/menu';
 
-export { Title, Button, Input, Textarea, Toggle, Radio, Breadcrumb };
+export {
+  Title,
+  Button,
+  Input,
+  Textarea,
+  Toggle,
+  Radio,
+  Breadcrumb,
+  Avatar,
+  AvatarGroup,
+  Menu,
+};
