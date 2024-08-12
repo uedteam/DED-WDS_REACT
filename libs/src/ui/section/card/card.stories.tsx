ee7b1c013dd775b437a842d7434f7909989a2b2a
Kevin <kk2229.yang@gmail.com>
2024-08-12 23:50:14 +0800

    🆙 update :  add aspect ratio parameter

--- libs/src/ui/section/card/card.stories.tsx
+++ libs/src/ui/section/card/card.stories.tsx
@@ -1,6 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
 import Card from './card';
+import { has } from 'lodash';
 
 export default {
   title: 'Design System/Section/Card',
@@ -33,10 +34,29 @@ export default {
         options: ['line', 'shadow'],
       },
     },
+    isFullImage: {
+      description: '是否滿版圖片',
+      control: {
+        type: 'boolean',
+      },
+    },
+    aspectRatio: {
+      description: '圖片比例',
+      control: {
+        type: 'radio',
+        options: ['11', '43', '54', '169'],
+      },
+    },
     className: {
       description: '標題樣式',
       control: {
         type: 'text',
+      },
+    },
+    hasMore: {
+      description: '是否有更多內容按鈕',
+      control: {
+        type: 'boolean',
       },
     },
     onMoreClick: {
@@ -66,31 +86,63 @@ export const Primary: Story = {
     subTitle: 'Card SubTitle',
     imageSrc: 'https://picsum.photos/320/240',
     isFullImage: false,
+    aspectRatio: '1:1',
     className: '',
-    onMoreClick: action('onClick'),
+    hasMore: true,
+    onMoreClick: (e) => {
+      action('onClick')(e);
+    },
   },
   render(args) {
     return (
-      <div style={{ maxWidth: '350px' }}>
+      <div style={{ width: 'auto' }}>
         <Card {...args} />
       </div>
     );
   },
 };
 
-export const PrimaryWithFullImage: Story = {
-  name: '滿版圖示卡片',
+export const PrimaryWithRow: Story = {
+  name: '滿版圖示橫排卡片',
   args: {
     // 設定參數預設值
     borderStyle: 'shadow',
+    cardTitle: 'Card Title',
     imageSrc: 'https://picsum.photos/320/240',
     isFullImage: true,
+    aspectRatio: '4:3',
     className: '',
+    hasMore: true,
     onMoreClick: action('onClick'),
   },
   render(args) {
     return (
-      <div style={{ maxWidth: '350px' }}>
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <Card {...args} />
+        <Card {...args} />
+        <Card {...args} />
+      </div>
+    );
+  },
+};
+
+export const PrimaryWithColumn: Story = {
+  name: '滿版圖示直排卡片',
+  args: {
+    // 設定參數預設值
+    borderStyle: 'shadow',
+    cardTitle: 'Card Title',
+    imageSrc: 'https://picsum.photos/320/240',
+    isFullImage: true,
+    aspectRatio: '4:3',
+    className: '',
+    hasMore: true,
+    onMoreClick: action('onClick'),
+  },
+  render(args) {
+    return (
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+        <Card {...args} />
         <Card {...args} />
       </div>
     );
@@ -103,11 +155,14 @@ export const TextCard: Story = {
     // 設定參數預設值
     cardTitle: 'Card Title',
     borderStyle: 'shadow',
+    aspectRatio: '1:1',
     className: '',
   },
   render(args) {
     return (
-      <div style={{ maxWidth: '350px' }}>
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <Card {...args} />
+        <Card {...args} />
         <Card {...args} />
       </div>
     );
--- libs/src/ui/section/card/card.tsx
+++ libs/src/ui/section/card/card.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { Title } from '../../element/title';
 import { Button } from '../../element/button';
-import { getBorderClass } from './styled';
+import { getBorderClass, getRadioClass } from './styled';
 import MoreIcon from '@assets/more_vert.svg?react';
 
 // 組件介面參數 props
 interface CardProps {
-  onMoreClick?: () => void;
+  onMoreClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   cardTitle?: string;
   subTitle?: string;
   imageSrc?: string;
+  hasMore?: boolean;
   isFullImage?: boolean;
+  aspectRatio: '1:1' | '4:3' | '5:4' | '16:9';
   borderStyle?: 'line' | 'shadow';
   className?: string;
 }
 
 // 定義組件
 // 修改組件定義名稱 Component
-export const Component: React.FC<CardProps> = (props: CardProps) => {
+export const Card: React.FC<CardProps> = (props: CardProps) => {
   // 解構組件參數
   const {
     onMoreClick,
     cardTitle,
     subTitle,
     imageSrc,
+    hasMore,
     isFullImage,
+    aspectRatio,
     borderStyle = 'line',
     className,
     ...rest
@@ -41,20 +45,12 @@ export const Component: React.FC<CardProps> = (props: CardProps) => {
         !isFullImage ? 'card-full-image' : ''
       }`}
     >
-      {cardTitle && (
-        <div className="card-header">
-          <div className="card-header-title">
-            <Title level={2}>{cardTitle}</Title>
-            <Button variant="text">
-              <MoreIcon className="card-icon"></MoreIcon>
-            </Button>
-          </div>
-          {subTitle && <div className="subtitle">{subTitle}</div>}
-        </div>
-      )}
-
       {imageSrc && (
-        <div className={`card-pic ${!isFullImage ? 'card-pic-padding' : ''}`}>
+        <div
+          className={`card-pic ${
+            !isFullImage ? 'card-pic-padding' : ''
+          } ${getRadioClass(aspectRatio)}`}
+        >
           <img
             className={`${!isFullImage ? 'image-rounded' : ''}`}
             src={imageSrc}
@@ -63,27 +59,41 @@ export const Component: React.FC<CardProps> = (props: CardProps) => {
         </div>
       )}
 
-      <div className="card-body">
-        <p className="card-desc">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s
-        </p>
-      </div>
-      {onMoreClick && (
-        <div className="card-footer">
-          <Button
-            {...rest}
-            onClick={onMoreClick}
-            variant="contained"
-            themeColor="primary"
-          >
-            Learn More
-          </Button>
+      <div className="card-container">
+        {cardTitle && (
+          <div className="card-header">
+            <div className="card-header-title">
+              <Title level={2}>{cardTitle}</Title>
+              <Button variant="text">
+                <MoreIcon className="card-icon"></MoreIcon>
+              </Button>
+            </div>
+            {subTitle && <div className="subtitle">{subTitle}</div>}
+          </div>
+        )}
+
+        <div className="card-body">
+          <p className="card-desc">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
         </div>
-      )}
+
+        {hasMore && (
+          <div className="card-footer">
+            <Button
+              {...rest}
+              onClick={onMoreClick}
+              variant="contained"
+              themeColor="primary"
+            >
+              Learn More
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
-// 修改匯出組件定義名稱 Component
-export default Component;
+export default Card;
