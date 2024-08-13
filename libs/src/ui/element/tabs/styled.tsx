d2f87799ccad611acc2b6c020b516b39c4b19132
Kevin <kk2229.yang@gmail.com>
2024-08-13 10:59:57 +0800

    🆙 update :  add tabs js doc

--- libs/src/ui/element/tabs/styled.tsx
+++ libs/src/ui/element/tabs/styled.tsx
@@ -1,3 +1,10 @@
+/**
+ * 根據主題顏色和類型獲取對應的樣式類別。
+ *
+ * @param {string} themeColor - 主題顏色。
+ * @param {string} type - 類型（例如 'card'）。
+ * @returns {string} 對應的樣式類別。
+ */
 export const getThemeClass = (themeColor: string, type: string) => {
   if (type === 'card') {
     return `tab-${type}-${themeColor}`;
@@ -5,13 +12,15 @@ export const getThemeClass = (themeColor: string, type: string) => {
   return `tab-${themeColor}`;
 };
 
+/**
+ * 根據主題顏色和類型返回活動類名。
+ * @param themeColor - 主題顏色。
+ * @param type - 標籤類型。
+ * @returns 活動類名。
+ */
 export const getActiveClass = (themeColor: string, type: string) => {
   if (type === 'card') {
     return `tab-${type}-${themeColor}-active`;
   }
   return `tab-${themeColor}-active`;
-};
-
-export const getDisableClass = () => {
-  return 'tab-disable';
 };
