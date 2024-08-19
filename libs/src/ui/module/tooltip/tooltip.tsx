dc8b54bf92c356ed870ad6d03917e840fbd87beb
Kevin <kk2229.yang@gmail.com>
2024-08-19 23:04:17 +0800

    🆙 update :  modify tooltip position hook

--- libs/src/ui/module/tooltip/tooltip.tsx
+++ libs/src/ui/module/tooltip/tooltip.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Portal from './portal';
+import React, { useState, useRef } from 'react';
+import Portal from '@src/ui/portal';
 import { getTargetPosition } from '@src/utils/position';
 import { getArrowPositionClass } from './styled';
+import { usePosition } from '@src/hooks/usePosition';
 
 interface TooltipProps {
   children: React.ReactNode;
@@ -27,51 +28,19 @@ export const Tooltip: React.FC<TooltipProps> = (props: TooltipProps) => {
   const { children, content, placement, showArrow, className } = props;
 
   const [isVisible, setIsVisible] = useState(false);
-  const childrenRef = useRef<HTMLSpanElement>(null);
-  const [childrenSize, setChildrenSize] = useState({
-    width: 0,
-    height: 0,
-  });
-  const [position, setPosition] = useState({
-    top: 0,
-    left: 0,
-  });
-
-  /* 事件控制 */
-  const handleOnResize = () => {
-    setChildrenSize({
-      width: childrenRef.current ? childrenRef.current.offsetWidth : 0,
-      height: childrenRef.current ? childrenRef.current.offsetHeight : 0,
-    });
-
-    setPosition({
-      top: childrenRef.current
-        ? childrenRef.current.getBoundingClientRect().top
-        : 0,
-      left: childrenRef.current
-        ? childrenRef.current.getBoundingClientRect().left
-        : 0,
-    });
-  };
-
-  useEffect(() => {
-    handleOnResize();
-    window.addEventListener('resize', handleOnResize);
-    return () => {
-      window.removeEventListener('resize', handleOnResize);
-    };
-  }, []);
+  const childrenRef = useRef<HTMLDivElement>(null);
+  const { childrenSize, position } = usePosition(childrenRef);
 
   return (
     <>
-      <span
+      <div
         ref={childrenRef}
         onMouseOver={() => setIsVisible(true)}
         onMouseLeave={() => setIsVisible(false)}
         className="tooltip-container"
       >
         {children}
-      </span>
+      </div>
       <Portal>
         {isVisible && (
           <div
