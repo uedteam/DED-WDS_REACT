3173ac3a4723e4a071cfe294cdfbf220f8b824c8
Kevin <kk2229.yang@gmail.com>
2024-10-04 21:42:53 +0800

    Updates

---
+++ assets/ChangeLog-DTIm9_-l.js
@@ -0,0 +1,23 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as i,am as m}from"./index-QrjJu2P3.js";import"./index-RYns6xqu.js";import"./iframe-BPgaz4Fv.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-B23dhaOI.js";import"./index-DrFu-skq.js";const a=`## V1.1.0 (Fri Oct 4 2024)
+
+✨ New Feature
+
+### Element
+
+### Module
+
+- Add Menu Story
+
+### Section
+
+- Add Side-Nav Story
+
+---
+
+🪲 Bug Fix
+`;function o(t){const e={h1:"h1",...r(),...t.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(i,{title:"Changelog"}),`
+`,n.jsx(e.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(m,{children:a})]})}function j(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
