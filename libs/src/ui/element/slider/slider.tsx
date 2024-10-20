67ac6abeb45f740cfd1ba9f2bf5cef39bc4428f9
Kevin <kk2229.yang@gmail.com>
2024-10-21 02:04:56 +0800

    🆙 update : modify slider component default

--- libs/src/ui/element/slider/slider.tsx
+++ libs/src/ui/element/slider/slider.tsx
@@ -1,6 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { getThemeClass } from './styled';
 
+/**
+ * 滑桿元件的屬性介面。
+ *
+ * @interface SliderProps
+ * @property {'primary' | 'secondary' | 'tertiary' | 'success' | 'warning' | 'error' | 'info'} [themeColor] - 主題顏色。
+ * @property {boolean} [isDisabled] - 是否禁用滑桿。
+ * @property {number} min - 最小值。
+ * @property {number} max - 最大值。
+ * @property {number} [step] - 每次變動的步長。
+ * @property {string} [unit] - 值的單位。
+ * @property {number} [initValue] - 初始值。
+ * @property {(value: number) => void} [onChange] - 當值變動時的回調函數。
+ * @property {string} [className] - 自訂樣式類名。
+ */
 export interface SliderProps {
   themeColor?:
     | 'primary'
@@ -14,21 +28,38 @@ export interface SliderProps {
   min: number;
   max: number;
   step?: number;
+  unit?: string;
   initValue?: number;
   onChange?: (value: number) => void;
-  unit?: string;
   className?: string;
 }
 
+/**
+ * Slider 元件
+ *
+ * @component
+ * @param {SliderProps} props - Slider 元件的屬性
+ * @param {string} [props.themeColor='primary'] - 主題顏色
+ * @param {boolean} [props.isDisabled=false] - 是否禁用 Slider
+ * @param {number} [props.min=0] - Slider 的最小值
+ * @param {number} [props.max=100] - Slider 的最大值
+ * @param {number} [props.step=1] - Slider 的步進值
+ * @param {string} [props.unit='%'] - Slider 值的單位
+ * @param {number} [props.initValue=0] - Slider 的初始值
+ * @param {function} [props.onChange] - 當 Slider 值改變時的回調函數
+ * @param {string} [props.className] - 自定義 CSS 類名
+ *
+ * @returns {JSX.Element} Slider 元件的 JSX
+ */
 export const Slider: React.FC<SliderProps> = ({
   themeColor = 'primary',
   isDisabled = false,
   min = 0,
   max = 100,
   step = 1,
+  unit = '℃',
   initValue = 0,
   onChange,
-  unit = '%',
   className,
 }) => {
   const [value, setValue] = useState<number>(initValue || min);
