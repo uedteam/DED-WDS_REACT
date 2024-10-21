d04f7e7dc8281f2995612ddc9ac776c8483f3122
Kevin <kk2229.yang@gmail.com>
2024-10-21 10:00:57 +0800

    🆙 update : modify layout component prefix class

--- libs/src/ui/section/layout/layout.tsx
+++ libs/src/ui/section/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 
 /* 組件介面參數 props */
 interface LayoutProps {
@@ -17,16 +17,7 @@ export const Layout: LayoutComponent = (props: LayoutProps) => {
   const { children, className, ...rest } = props;
 
   return (
-    <div
-      style={{
-        width: '100%',
-        display: 'flex',
-        flexWrap: 'wrap',
-        flex: '1',
-        color: '#fff',
-      }}
-      {...rest}
-    >
+    <div className="ded-layout" {...rest}>
       {children}
     </div>
   );
@@ -38,20 +29,7 @@ interface HeaderProps {
 const header = (props: HeaderProps) => {
   const { children } = props;
 
-  return (
-    <header
-      style={{
-        width: '100%',
-        height: '40px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#4096ff',
-      }}
-    >
-      {children}
-    </header>
-  );
+  return <header className="ded-header">{children}</header>;
 };
 
 interface FooterProps {
@@ -60,20 +38,7 @@ interface FooterProps {
 const footer = (props: FooterProps) => {
   const { children } = props;
 
-  return (
-    <footer
-      style={{
-        width: '100%',
-        height: '40px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#4096ff',
-      }}
-    >
-      {children}
-    </footer>
-  );
+  return <footer className="ded-footer">{children}</footer>;
 };
 
 interface ContentProps {
@@ -82,20 +47,7 @@ interface ContentProps {
 const content = (props: ContentProps) => {
   const { children } = props;
 
-  return (
-    <main
-      style={{
-        flex: '1',
-        height: '80px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#0958d9',
-      }}
-    >
-      {children}
-    </main>
-  );
+  return <main className="ded-content">{children}</main>;
 };
 
 interface SideProps {
@@ -103,19 +55,7 @@ interface SideProps {
 }
 const side = (props: SideProps) => {
   const { children } = props;
-  return (
-    <aside
-      style={{
-        width: '80px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#1677ff',
-      }}
-    >
-      {children}
-    </aside>
-  );
+  return <aside className="ded-side">{children}</aside>;
 };
 
 Layout.Header = header;
