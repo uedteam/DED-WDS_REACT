b8ac2ab1b933309cdd2d08db2ddefcb707b39e5d
Kevin <kk2229.yang@gmail.com>
2025-02-05 20:25:32 +0800

    Updates

--- assets/preview-BcJquUPu.js
+++
@@ -1,1 +0,0 @@
-const a={initialGlobals:{styles:{body:{margin:0,padding:0,fontFamily:"Arial, sans-serif"}}},parameters:{backgrounds:{values:[{name:"light",value:"#fff"},{name:"dark",value:"#333"},{name:"gray",value:"#f0f0f0"}]},options:{storySort:{method:"alphabetical",order:["Introduction","Components"],locales:"en-US"}}},tags:["autodocs"]};export{a as default};
