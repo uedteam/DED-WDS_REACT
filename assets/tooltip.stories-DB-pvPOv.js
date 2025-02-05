bfdbcc8ba7156d36805975dcf423a60773a69316
Kevin <kk2229.yang@gmail.com>
2025-02-05 20:35:08 +0800

    Updates

---
+++ assets/tooltip.stories-DB-pvPOv.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{c as p}from"./tooltip-BUQqBuwY.js";import"./badge-vsbKzH_B.js";import"./breadcrumb-CjMUm_Kh.js";import{B as s}from"./toast-CLZ2ghKy.js";import"./checkbox-wrczPlMV.js";import"./progress-circle-xnhIhzfk.js";import"./progress-line-DsOpskWp.js";import"./divider-ChKDBDeF.js";import"./grid-D9dEAlnk.js";import"./image-DG0n6Q3C.js";import"./input-CipjVc3A.js";import"./layout-ZjbFUAzr.js";import"./menu-DAvbBviu.js";import"./radio-CMOPLxLt.js";import"./status-indicator-BDww4a8h.js";import"./tag-Code_Ef5.js";import"./textarea-CO8Z8fZG.js";import"./title-Cw6Qriwq.js";import"./toggle-Ck5Sa1oo.js";import{b as m}from"./warning-tri-Y1-2BlEi.js";import"./index-RYns6xqu.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./notification-NM2mg_B2.js";import"./index-sbqOYYIm.js";import"./check-BnJBHFpS.js";import"./useCounter-DpfJ3ATo.js";const L={title:"Component/Tooltip",component:p,tags:["autodocs"],argTypes:{content:{description:"提示內容",table:{category:"PROPS"}},showArrow:{description:"是否顯示箭頭",table:{category:"PROPS"}},placement:{description:"提示框位置",control:{type:"select",options:["top-left","top","top-right","right-top","right","right-bottom","bottom-right","bottom","bottom-left","left-bottom","left","left-top"]},table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},children:{description:"顯示內容",table:{category:"SLOTS"}}},args:{content:"good day",placement:"top",showArrow:!0,className:"",children:o.jsx(s,{prefix:o.jsx(m,{}),themeColor:"primary",variant:"filled",onClick:()=>({}),children:"Hover Me"})},parameters:{docs:{title:"Tooltip",description:{component:"提示工具組件的呈現及說明。"}}}},t={name:"預設項目",args:{},render(n){return o.jsx("div",{style:{display:"flex",justifyContent:"center"},children:o.jsx(p,{...n})})}};var r,e,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <div style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+        <Tooltip {...args}></Tooltip>
+      </div>;
+  }
+}`,...(i=(e=t.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const M=["Default"];export{t as Default,M as __namedExportsOrder,L as default};
