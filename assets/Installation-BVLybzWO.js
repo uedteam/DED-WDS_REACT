6a7c9c207641ae0b83a6ebd7c300635178ded83c
Kevin <kk2229.yang@gmail.com>
2025-02-05 20:32:39 +0800

    Updates

--- assets/Installation-BVLybzWO.js
+++ assets/Installation-BVLybzWO.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import{ae as i,am as p}from"./index-C0Z3uvqh.js";import"./index-RYns6xqu.js";import"./iframe-VHu0b1pD.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const r=`# 關於 Web Design system Storybook
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as o}from"./index-CcnH5Kt0.js";import{ae as i,am as p}from"./index-iJlSIAcN.js";import"./index-RYns6xqu.js";import"./iframe-COPaMyTI.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const r=`# 關於 Web Design system Storybook
 
 ## 關於 AUO Design system
 
