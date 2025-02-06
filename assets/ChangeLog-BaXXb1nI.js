dce73df9cdd1439c47cd1e01146afe5054a1d6a3
Kevin <kk2229.yang@gmail.com>
2025-02-06 15:58:14 +0800

    Updates

---
+++ assets/ChangeLog-BaXXb1nI.js
@@ -0,0 +1,19 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import{ae as i,am as a}from"./index-DcQfWpXo.js";import"./index-RYns6xqu.js";import"./iframe-DwgltxS-.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./index-BwO1pRjM.js";import"./index-DrFu-skq.js";const s=`## V1.0.3-beta (2025/2/5)
+
+✨ New Feature
+
+🆙 Update
+
+- Add the controls property with expanded: true in the preview.ts file
+- Update the name and version in the package-lock.json file
+- Add class-variance-authority as a dependency in the package-lock.json file
+- Update the import path for SvgArrowDown in the accordion-item.tsx file
+- Update the import path for SvgArrowDown in the accordion.stories.tsx file
+
+🪲 Bug Fix
+`;function o(e){const t={h1:"h1",...r(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(i,{title:"Introduction/Changelog"}),`
+`,n.jsx(t.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(a,{children:s})]})}function g(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{g as default};
---
+++ assets/tooltip.stories-8VGkjgAW.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{c as p}from"./tooltip-BkWWl-FY.js";import"./badge-BPltvF_h.js";import"./breadcrumb-DSLqi4-x.js";import{B as s}from"./toast-DwcNDvV5.js";import"./checkbox-BnGhXkr7.js";import"./progress-circle-5ZOi_8dX.js";import"./progress-line-BURSIgt3.js";import"./divider-ChKDBDeF.js";import"./grid-D9dEAlnk.js";import"./image-DG0n6Q3C.js";import"./input-D3ifh5CR.js";import"./layout-ZjbFUAzr.js";import"./menu-DNbdc4Xz.js";import"./radio-CMOPLxLt.js";import"./status-indicator-BDww4a8h.js";import"./tag-W9QNdB38.js";import"./textarea-CO8Z8fZG.js";import"./title-BdHUK5iL.js";import"./toggle-Ck5Sa1oo.js";import{b as m}from"./warning-tri-BY3sPlw-.js";import"./index-RYns6xqu.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./index-sbqOYYIm.js";import"./check-BnJBHFpS.js";import"./useCounter-DpfJ3ATo.js";const H={title:"Component/Tooltip",component:p,tags:["autodocs"],argTypes:{content:{description:"提示內容",table:{category:"PROPS"}},showArrow:{description:"是否顯示箭頭",table:{category:"PROPS"}},placement:{description:"提示框位置",control:{type:"select",options:["top-left","top","top-right","right-top","right","right-bottom","bottom-right","bottom","bottom-left","left-bottom","left","left-top"]},table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},children:{description:"顯示內容",table:{category:"SLOTS"}}},args:{content:"good day",placement:"top",showArrow:!0,className:"",children:o.jsx(s,{prefix:o.jsx(m,{}),themeColor:"primary",variant:"filled",onClick:()=>({}),children:"Hover Me"})},parameters:{docs:{title:"Tooltip",description:{component:"提示工具組件的呈現及說明。"}}}},t={name:"預設項目",args:{},render(n){return o.jsx("div",{style:{display:"flex",justifyContent:"center"},children:o.jsx(p,{...n})})}};var r,e,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <div style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+        <Tooltip {...args}></Tooltip>
+      </div>;
+  }
+}`,...(i=(e=t.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const L=["Default"];export{t as Default,L as __namedExportsOrder,H as default};
