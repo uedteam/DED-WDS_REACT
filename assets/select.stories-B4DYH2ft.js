a429b5b6053ee15f1d15c9b84d9eba59e57b2cde
Kevin <kk2229.yang@gmail.com>
2025-02-10 15:42:17 +0800

    Updates

---
+++ assets/select.stories-B4DYH2ft.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-DEdD30eg.js";import{a as m}from"./chunk-454WOBUV-ChHd1ABr.js";import{S as i}from"./tooltip-BaPqchHM.js";import"./badge-BPltvF_h.js";import"./breadcrumb-D9hZ6nwX.js";import"./toast-_qk7dVr8.js";import"./checkbox-BnGhXkr7.js";import"./progress-circle-5ZOi_8dX.js";import"./progress-line-2T3fy2De.js";import"./divider-DxMrCkxg.js";import"./grid-D9dEAlnk.js";import"./image-BV9A8HRV.js";import"./input-pt8Ow9OS.js";import"./layout-DgebEeDV.js";import"./menu-CZmZ_eer.js";import"./radio-CMOPLxLt.js";import"./status-indicator-CulQZKO6.js";import"./tag-W9QNdB38.js";import"./textarea-CO8Z8fZG.js";import"./title-BdHUK5iL.js";import"./toggle-Ck5Sa1oo.js";import{i as u}from"./warning-tri-BY3sPlw-.js";import{r as d}from"./index-RYns6xqu.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./index-sbqOYYIm.js";import"./check-BnJBHFpS.js";import"./useCounter-DpfJ3ATo.js";const g=[{value:"1",label:"Option 1"},{value:"2",label:"Option 2"},{value:"3",label:"Option 3"}],J={title:"Component/Select",component:i,tags:["autodocs"],argTypes:{dataSource:{description:"選項",control:{type:"object"},table:{category:"PROPS",type:{detail:`
+            interface Option {
+              label: string;
+              value: string | number;
+            }
+          `}}},suffix:{description:"後綴圖示",table:{category:"PROPS"}},placeholder:{description:"輸入提示",table:{category:"PROPS"}},value:{description:"值",table:{category:"PROPS"}},isDisabled:{description:"是否禁用",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onChange:{description:"選擇變更事件",table:{category:"EVENTS"}}},args:{dataSource:g,placeholder:"Select",value:"",suffix:n.jsx(u,{width:18,height:18}),isDisabled:!1,className:""},parameters:{docs:{title:"Select",description:{component:"選擇組件的呈現及說明。"}}}},e={name:"預設項目",args:{},render:function(s){const[l,c]=d.useState(""),p=t=>{m("onChange")(t),c(t)};return n.jsx(i,{...s,value:l,onChange:p})}};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render: function (args) {
+    const [selectedValue, setSelectedValue] = useState<string | number>('');
+    const handleChange = (value: string | number) => {
+      action('onChange')(value);
+      setSelectedValue(value);
+    };
+    return <Select {...args} value={selectedValue} onChange={handleChange} />;
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const K=["Default"];export{e as Default,K as __namedExportsOrder,J as default};
