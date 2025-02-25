a1a99a75658127a700afdb6e3af0e49eb654f911
Kevin <kk2229.yang@gmail.com>
2025-02-25 13:18:29 +0800

    Updates

---
+++ assets/avatar.stories-DjcK7hsC.js
@@ -0,0 +1,88 @@
+import{j as a}from"./jsx-runtime-DEdD30eg.js";import{a as r}from"./tooltip-MID_d6V_.js";import"./index-RYns6xqu.js";import"./warning-tri-D1aSkfBJ.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./badge-BPltvF_h.js";import"./breadcrumb-BT-H3bo-.js";import"./toast-BhiYnD6v.js";import"./close-o-4V2u0k.js";import"./title-BdHUK5iL.js";import"./index-sbqOYYIm.js";import"./checkbox-Oor9e7gG.js";import"./check-BnJBHFpS.js";import"./progress-circle-E3pfpkaU.js";import"./useCounter-DpfJ3ATo.js";import"./progress-line-DvRcyvhi.js";import"./divider-DxMrCkxg.js";import"./grid-D9dEAlnk.js";import"./image-B9gne3T0.js";import"./input-CMbRWpBm.js";import"./layout-DgebEeDV.js";import"./menu-Bywynte6.js";import"./radio-CfSeqhQ5.js";import"./status-indicator-Cbvkr_-o.js";import"./tag-gKTSkn4a.js";import"./textarea-r7CXvhUd.js";import"./toggle-Ck5Sa1oo.js";const K={title:"Component/Avatar",component:r,tags:["autodocs"],argTypes:{shape:{description:"形狀",control:{type:"select",options:["circle","square"]},table:{category:"PROPS"}},size:{description:"尺寸",control:{type:"select",options:["small","medium","large"]},table:{category:"PROPS"}},status:{description:"狀態",control:{type:"select",options:["none","online","idle","busy","offline"]},table:{category:"PROPS"}},isShowInfo:{description:"是否顯示資訊",table:{category:"PROPS"}},src:{description:"圖片連結",table:{category:"PROPS"}},alt:{description:"圖片描述",table:{category:"PROPS"}},userName:{description:"使用者名稱",table:{category:"PROPS"}},caption:{description:"使用者描述",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}}},decorators:[e=>a.jsx("div",{style:{display:"flex",gap:"32px",alignItems:"flex-end"},children:a.jsx(e,{})})],parameters:{docs:{title:"Avatar",description:{component:"個人頭像組件的呈現及說明。"}}},args:{shape:"circle",size:"large",status:"none",isShowInfo:!0,src:"https://storage.googleapis.com/ded-wds-bucket/monkey.png",alt:"無圖顯示",userName:"Name",caption:"Caption",className:""}},t={name:"預設項目",args:{},render(e){return a.jsx(r,{...e})}},s={name:"頭像形狀",argTypes:{shape:{table:{disable:!0}},alt:{table:{disable:!0}},src:{table:{disable:!0}}},args:{src:"https://storage.googleapis.com/ded-wds-bucket/fox.png"},parameters:{docs:{source:{code:`
+        <Avatar {...args} shape="circle" />
+<Avatar {...args} shape="square" />
+        `}}},render(e){return a.jsxs(a.Fragment,{children:[a.jsx(r,{...e,shape:"circle"}),a.jsx(r,{...e,shape:"square"})]})}},o={name:"頭像狀態",argTypes:{status:{table:{disable:!0}}},args:{shape:"circle",size:"large",src:"https://storage.googleapis.com/ded-wds-bucket/tigger.png"},parameters:{docs:{source:{code:`
+<Avatar {...args} status="online" />
+<Avatar {...args} status="idle" />
+<Avatar {...args} status="busy" />
+<Avatar {...args} status="offline" />
+        `}}},render(e){return a.jsxs(a.Fragment,{children:[a.jsx(r,{...e}),a.jsx(r,{...e,status:"online"}),a.jsx(r,{...e,status:"idle"}),a.jsx(r,{...e,status:"busy"}),a.jsx(r,{...e,status:"offline"})]})}};var i,p,c;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <Avatar {...args} />;
+  }
+}`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var n,l,g;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: '頭像形狀',
+  argTypes: {
+    shape: {
+      table: {
+        disable: true
+      }
+    },
+    alt: {
+      table: {
+        disable: true
+      }
+    },
+    src: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  args: {
+    src: 'https://storage.googleapis.com/ded-wds-bucket/fox.png'
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+        <Avatar {...args} shape="circle" />
+<Avatar {...args} shape="square" />
+        \`
+      }
+    }
+  },
+  render(args) {
+    return <>
+        <Avatar {...args} shape="circle" />
+        <Avatar {...args} shape="square" />
+      </>;
+  }
+}`,...(g=(l=s.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var m,d,u;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  name: '頭像狀態',
+  argTypes: {
+    status: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  args: {
+    shape: 'circle',
+    size: 'large',
+    src: 'https://storage.googleapis.com/ded-wds-bucket/tigger.png'
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`
+<Avatar {...args} status="online" />
+<Avatar {...args} status="idle" />
+<Avatar {...args} status="busy" />
+<Avatar {...args} status="offline" />
+        \`
+      }
+    }
+  },
+  render(args) {
+    return <>
+        <Avatar {...args} />
+        <Avatar {...args} status="online" />
+        <Avatar {...args} status="idle" />
+        <Avatar {...args} status="busy" />
+        <Avatar {...args} status="offline" />
+      </>;
+  }
+}`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const L=["Default","AvatarShape","AvatarStatus"];export{s as AvatarShape,o as AvatarStatus,t as Default,L as __namedExportsOrder,K as default};
---
+++ assets/tabs.stories-C75Wocyb.js
@@ -0,0 +1,86 @@
+import{j as a}from"./jsx-runtime-DEdD30eg.js";import{a as P}from"./chunk-D5ZWXAHU-CGd0bvzi.js";import{r as T}from"./index-RYns6xqu.js";import"./tooltip-MID_d6V_.js";import"./badge-BPltvF_h.js";import"./breadcrumb-BT-H3bo-.js";import{B as x}from"./toast-BhiYnD6v.js";import"./checkbox-Oor9e7gG.js";import"./progress-circle-E3pfpkaU.js";import"./progress-line-DvRcyvhi.js";import"./divider-DxMrCkxg.js";import"./grid-D9dEAlnk.js";import"./image-B9gne3T0.js";import"./input-CMbRWpBm.js";import"./layout-DgebEeDV.js";import"./menu-Bywynte6.js";import"./radio-CfSeqhQ5.js";import"./status-indicator-Cbvkr_-o.js";import"./tag-gKTSkn4a.js";import"./textarea-r7CXvhUd.js";import"./title-BdHUK5iL.js";import"./toggle-Ck5Sa1oo.js";import{a as h}from"./warning-tri-D1aSkfBJ.js";import"./string-BqrfsRv8.js";import"./online-EmbACc0H.js";import"./close-o-4V2u0k.js";import"./index-sbqOYYIm.js";import"./check-BnJBHFpS.js";import"./useCounter-DpfJ3ATo.js";const y=(e,t)=>t==="basic"?`ded-tab-${e}`:`ded-tab-${t}-${e}`,g=(e,t)=>t==="basic"?`ded-tab-${e}-active`:`ded-tab-${t}-${e}-active`;try{y.displayName="getThemeClass",y.__docgenInfo={description:"根據主題顏色和類型獲取對應的樣式類別。",displayName:"getThemeClass",props:{}}}catch{}try{g.displayName="getActiveClass",g.__docgenInfo={description:"根據主題顏色和類型返回活動類名。",displayName:"getActiveClass",props:{}}}catch{}const v=({themeColor:e="primary",type:t="basic",title:n="",prefix:s="",isActive:l,isDisabled:o=!1,className:c="",onClick:u})=>t==="button"?a.jsxs(x,{className:`
+      ${l?g(e,t):"ded-tab-button"}
+      ${o?"ded-tab-button-disable":c||y(e,t)}`,variant:"filled",onClick:u,children:[s&&a.jsx("span",{className:"ded-icon",children:s}),a.jsx("span",{children:n})]}):a.jsxs(x,{className:`ded-tab ${l?g(e,t):""} 
+      ${o?"ded-tab-disable":c||y(e,t)}`,variant:"text",onClick:u,children:[s&&a.jsx("span",{className:"ded-icon",children:s}),a.jsx("span",{children:n})]});try{v.displayName="TabItem",v.__docgenInfo={description:"TabItem 組件代表標籤組中的單個標籤項目。",displayName:"TabItem",props:{themeColor:{defaultValue:{value:"primary"},description:"",name:"themeColor",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"neutral"'},{value:'"info"'},{value:'"success"'},{value:'"warning"'},{value:'"error"'}]}},type:{defaultValue:{value:"basic"},description:"",name:"type",required:!1,type:{name:"enum",value:[{value:'"basic"'},{value:'"outline"'},{value:'"button"'}]}},title:{defaultValue:{value:""},description:"",name:"title",required:!1,type:{name:"string"}},prefix:{defaultValue:{value:""},description:"",name:"prefix",required:!1,type:{name:"ReactNode"}},isActive:{defaultValue:null,description:"",name:"isActive",required:!0,type:{name:"boolean"}},isDisabled:{defaultValue:{value:"false"},description:"",name:"isDisabled",required:!1,type:{name:"boolean"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!0,type:{name:"(event: MouseEvent<HTMLButtonElement, MouseEvent>) => void"}},className:{defaultValue:{value:""},description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const r=({themeColor:e="primary",dataSource:t,activeIndex:n=0,type:s="basic",prefix:l="",isDisabled:o=!1,className:c="",onClick:u})=>{const[f,C]=T.useState(0),A=(d,i)=>{C(i),d.currentTarget.blur(),u&&u(d)};return T.useEffect(()=>{C(n||0)},[n]),a.jsxs("div",{className:`ded-tabs-container ${c}`,children:[a.jsx("div",{className:`ded-tabs ${s==="button"?"ded-tabs-button":""}`,children:t.map((d,i)=>a.jsx(v,{title:d.title,themeColor:e,type:s,prefix:l,isDisabled:o,isActive:i===f,onClick:B=>A(B,i)},i))}),a.jsx("div",{className:`ded-tab-content ${o&&"ded-tab-disable"}`,children:t[f].content})]})};try{r.displayName="Tabs",r.__docgenInfo={description:"渲染帶有相關內容的標籤組。",displayName:"Tabs",props:{themeColor:{defaultValue:{value:"primary"},description:"",name:"themeColor",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"neutral"'},{value:'"info"'},{value:'"success"'},{value:'"warning"'},{value:'"error"'}]}},dataSource:{defaultValue:null,description:"",name:"dataSource",required:!0,type:{name:"{ title: string; content: string; }[]"}},activeIndex:{defaultValue:{value:"0"},description:"",name:"activeIndex",required:!1,type:{name:"number"}},type:{defaultValue:{value:"basic"},description:"",name:"type",required:!1,type:{name:"enum",value:[{value:'"basic"'},{value:'"outline"'},{value:'"button"'}]}},prefix:{defaultValue:{value:""},description:"",name:"prefix",required:!1,type:{name:"ReactNode"}},isDisabled:{defaultValue:{value:"false"},description:"",name:"isDisabled",required:!1,type:{name:"boolean"}},className:{defaultValue:{value:""},description:"",name:"className",required:!1,type:{name:"string"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"((event: MouseEvent<HTMLButtonElement, MouseEvent>) => void)"}}}}}catch{}try{tabs.displayName="tabs",tabs.__docgenInfo={description:"渲染帶有相關內容的標籤組。",displayName:"tabs",props:{themeColor:{defaultValue:{value:"primary"},description:"",name:"themeColor",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"neutral"'},{value:'"info"'},{value:'"success"'},{value:'"warning"'},{value:'"error"'}]}},dataSource:{defaultValue:null,description:"",name:"dataSource",required:!0,type:{name:"{ title: string; content: string; }[]"}},activeIndex:{defaultValue:{value:"0"},description:"",name:"activeIndex",required:!1,type:{name:"number"}},type:{defaultValue:{value:"basic"},description:"",name:"type",required:!1,type:{name:"enum",value:[{value:'"basic"'},{value:'"outline"'},{value:'"button"'}]}},prefix:{defaultValue:{value:""},description:"",name:"prefix",required:!1,type:{name:"ReactNode"}},isDisabled:{defaultValue:{value:"false"},description:"",name:"isDisabled",required:!1,type:{name:"boolean"}},className:{defaultValue:{value:""},description:"",name:"className",required:!1,type:{name:"string"}},onClick:{defaultValue:null,description:"",name:"onClick",required:!1,type:{name:"((event: MouseEvent<HTMLButtonElement, MouseEvent>) => void)"}}}}}catch{}const w=[{title:"Tab 1",content:"Content 1"},{title:"Tab 2",content:"Content 2"},{title:"Tab 3",content:"Content 3"}],de={title:"Component/Tabs",component:r,tags:["autodocs"],argTypes:{themeColor:{description:"主題顏色",control:{type:"select",options:["primary","secondary","neutral","info","success","warning","error"]},table:{category:"PROPS"}},dataSource:{description:"資料來源",table:{category:"PROPS"}},activeIndex:{description:"活動頁籤索引",control:{type:"number",min:0,max:2},table:{category:"PROPS"}},type:{description:"頁籤樣式",control:{type:"select",options:["basic","outline","button"]},table:{category:"PROPS"}},prefix:{description:"前綴元素",options:["none","SvgArrowDown"],mapping:{none:null,SvgArrowDown:a.jsx(h,{})},table:{category:"PROPS"}},isDisabled:{description:"是否禁用",table:{category:"PROPS"}},className:{description:"客製化樣式",table:{category:"PROPS"}},onClick:{description:"點擊事件",action:"clicked",table:{category:"EVENTS"}}},args:{themeColor:"primary",dataSource:w,activeIndex:0,type:"basic",prefix:a.jsx(h,{}),isDisabled:!1,className:"",onClick:P("onClick")},decorators:[e=>a.jsx("div",{style:{display:"flex",flexDirection:"column",gap:"32px"},children:a.jsx(e,{})})],parameters:{docs:{title:"Tabs",description:{component:"頁籤組件的呈現及說明。"}}}},m={name:"預設項目",args:{},render(e){return a.jsx(r,{...e})}},p={name:"頁籤類型",argTypes:{type:{table:{disable:!0}}},args:{},parameters:{docs:{source:{transform(e,t){return`
+<Tabs {...args} type="basic" />
+<Tabs {...args} type="outline" />
+<Tabs {...args} type="button" />
+`}}}},render(e){return a.jsxs(a.Fragment,{children:[a.jsx(r,{...e,type:"basic"}),a.jsx(r,{...e,type:"outline"}),a.jsx(r,{...e,type:"button"})]})}},b={name:"主題色彩",args:{type:"button",activeIndex:2},parameters:{docs:{source:{transform(e,t){return`
+<Tabs {...args} themeColor="primary" />
+<Tabs {...args} themeColor="secondary" />
+<Tabs {...args} themeColor="neutral" />
+<Tabs {...args} themeColor="info" />
+<Tabs {...args} themeColor="success" />
+<Tabs {...args} themeColor="warning" />
+<Tabs {...args} themeColor="error" />
+`}}}},render(e){return a.jsxs(a.Fragment,{children:[a.jsx(r,{...e,themeColor:"primary"}),a.jsx(r,{...e,themeColor:"secondary"}),a.jsx(r,{...e,themeColor:"neutral"}),a.jsx(r,{...e,themeColor:"info"}),a.jsx(r,{...e,themeColor:"success"}),a.jsx(r,{...e,themeColor:"warning"}),a.jsx(r,{...e,themeColor:"error"})]})}};var _,E,D;m.parameters={...m.parameters,docs:{...(_=m.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  name: '預設項目',
+  args: {},
+  render(args) {
+    return <Tabs {...args} />;
+  }
+}`,...(D=(E=m.parameters)==null?void 0:E.docs)==null?void 0:D.source}}};var j,N,V;p.parameters={...p.parameters,docs:{...(j=p.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  name: '頁籤類型',
+  argTypes: {
+    type: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  args: {},
+  parameters: {
+    docs: {
+      source: {
+        transform(code: string, storyContext: StoryContext) {
+          return \`
+<Tabs {...args} type="basic" />
+<Tabs {...args} type="outline" />
+<Tabs {...args} type="button" />
+\`;
+        }
+      }
+    }
+  },
+  render(args) {
+    return <>
+        <Tabs {...args} type="basic" />
+        <Tabs {...args} type="outline" />
+        <Tabs {...args} type="button" />
+      </>;
+  }
+}`,...(V=(N=p.parameters)==null?void 0:N.docs)==null?void 0:V.source}}};var q,S,I;b.parameters={...b.parameters,docs:{...(q=b.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  name: '主題色彩',
+  args: {
+    type: 'button',
+    activeIndex: 2
+  },
+  parameters: {
+    docs: {
+      source: {
+        transform(code: string, storyContext: StoryContext) {
+          return \`
+<Tabs {...args} themeColor="primary" />
+<Tabs {...args} themeColor="secondary" />
+<Tabs {...args} themeColor="neutral" />
+<Tabs {...args} themeColor="info" />
+<Tabs {...args} themeColor="success" />
+<Tabs {...args} themeColor="warning" />
+<Tabs {...args} themeColor="error" />
+\`;
+        }
+      }
+    }
+  },
+  render(args) {
+    return <>
+        <Tabs {...args} themeColor="primary" />
+        <Tabs {...args} themeColor="secondary" />
+        <Tabs {...args} themeColor="neutral" />
+        <Tabs {...args} themeColor="info" />
+        <Tabs {...args} themeColor="success" />
+        <Tabs {...args} themeColor="warning" />
+        <Tabs {...args} themeColor="error" />
+      </>;
+  }
+}`,...(I=(S=b.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};const me=["Default","Type","PrimaryWithButton"];export{m as Default,b as PrimaryWithButton,p as Type,me as __namedExportsOrder,de as default};
