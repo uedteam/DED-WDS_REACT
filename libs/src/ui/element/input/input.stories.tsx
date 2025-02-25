4aba1435950c421d6fba25c6117c6126114e65bf
uedteam <auo.uedteam@gmail.com>
2025-02-25 09:48:51 +0800

    refactor: refactor input and select components' event handling (#103)
    - Remove unused metadata related to the `name` property in the Input
    story component
    - Modify the `onChange` event in the Input story component to take a
    `ChangeEvent` parameter
    - Remove unused metadata related to the `name` property in the Input
    component
    - Modify the `onChange` event in the Input component to take a
    `ChangeEvent` parameter
    - Modify the `onChange` event in the Select component to take a
    `ChangeEvent` parameter
    
    Signed-off-by: Kevin <kk2229.yang@gmail.com>
    Co-authored-by: Kevin <kk2229.yang@gmail.com>

--- libs/src/ui/element/input/input.stories.tsx
+++ libs/src/ui/element/input/input.stories.tsx
@@ -9,6 +9,7 @@ import {
   SvgClose,
   SvgLock,
 } from '@src/assets/icons';
+import { ChangeEvent } from 'react';
 
 const IconComponents = {
   SvgAccount: <SvgAccount />,
@@ -26,12 +27,6 @@ export default {
   argTypes: {
     label: {
       description: '標題',
-      table: {
-        category: 'PROPS',
-      },
-    },
-    name: {
-      description: '名稱',
       table: {
         category: 'PROPS',
       },
@@ -133,18 +128,6 @@ export default {
         category: 'EVENTS',
       },
     },
-    onFocus: {
-      description: '焦點事件',
-      table: {
-        category: 'EVENTS',
-      },
-    },
-    onBlur: {
-      description: '失焦事件',
-      table: {
-        category: 'EVENTS',
-      },
-    },
   },
   parameters: {
     docs: {
@@ -157,7 +140,6 @@ export default {
   args: {
     label: 'Label',
     type: 'text',
-    name: 'input',
     hasClear: true,
     placeholder: 'Placeholder',
     size: 'medium',
@@ -166,9 +148,7 @@ export default {
     hint: { error: '', description: 'Prompt message' },
     isDisabled: false,
     className: '',
-    onChange: (e: string) => action('changed')(e),
-    onFocus: (e: string) => action('focused')(e),
-    onBlur: (e: string) => action('blured')(e),
+    onChange: (e: ChangeEvent) => action('changed')(e.target),
   },
 } as Meta;
 type Story = StoryObj<typeof Input>;
@@ -179,7 +159,8 @@ export const Default: Story = {
     prefix: <SvgAccount />,
   },
   render(args) {
-    return <Input {...args} />;
+    const args2 = { test: '123', name: 'Kevin' };
+    return <Input {...args} {...args2} />;
   },
 };
 
